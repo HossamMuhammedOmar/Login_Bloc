f44dfe1fa030fa5451f8bb66c6dcd89c0eb40d2f
HossamMuhammedOmar <hossamomar9876@gmail.com>
2020-11-03 07:32:26 +0200

    create validator class

---
+++ lib/src/blocs/validators.dart
@@ -0,0 +1,13 @@
+import 'dart:async';
+
+class Validators {
+  final validateEmail = new StreamTransformer<String, String>.fromHandlers(
+    handleData: (email, sink) {
+      if (email.contains('@')) {
+        sink.add(email);
+      } else {
+        sink.addError('Enter a valid email');
+      }
+    },
+  );
+}
